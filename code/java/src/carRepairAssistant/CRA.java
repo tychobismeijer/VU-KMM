6aca6a8706d516b06247a647751f352515a34af1
Tycho Bismeijer <tycho@tycho-pc-debian.tycho.local>
2010-12-11 15:12:03 +0100

    merged changes from joost

--- code/java/src/carRepairAssistant/CRA.java
+++ code/java/src/carRepairAssistant/CRA.java
@@ -5,10 +5,10 @@ import jess.JessException;
 import jess.WorkingMemoryMarker;
 import java.io.Console;
 
-public class CRA {
+public class CRAjoost {
 
     private Rete jess;
-    private Console c;
+    private ConsoleCheat c;
     private String
         currentHypothesisComponent,
         currentHypothesisState;
@@ -24,9 +24,61 @@ public class CRA {
         }
     }
 
-    private boolean selectHypothesis() throws JessException {
-        jess.QueryResult hypothesis =
-            jess.runQueryStar("search-hypothesis", new jess.ValueVector());
+    private void askAvailableHypothesis() throws JessException{
+        String[][] allHypothesis = allHypothesis();
+        c.printf("Choose an hypothesis:\n");
+        for(int i=0; i<allHypothesis.length; i++){
+            c.printf(i + " " + allHypothesis[i][0] + " is " + allHypothesis[i][1] + "\n");
+        }
+        String choice = c.readLine();
+
+        choice = choice.trim();
+        if (isNumber(choice)){
+            int nrChoice = Integer.parseInt(choice);
+            currentHypothesisComponent = allHypothesis[nrChoice][0];
+            currentHypothesisState = allHypothesis[nrChoice][1];
+        } else if(choice.contains(" is ")){
+            currentHypothesisComponent = choice.substring(0, choice.indexOf(" "));
+            currentHypothesisState = choice.substring(choice.lastIndexOf(" ")+1,choice.length());
+        }
+    }
+
+    private boolean isNumber(String string){
+        try{
+            Integer.parseInt(string);
+            return true;
+        }
+        catch(NumberFormatException nfe){
+            return false;
+        }
+    }
+
+    private String[][] allHypothesis() throws JessException{
+        int nrHypothesis = countHypothesis();
+        String[][] result = new String[nrHypothesis][2];
+        jess.QueryResult hypothesis = generateHypothesis();
+        for(int i = 0; i<nrHypothesis; i++){
+            selectHypothesis(hypothesis);
+            result[i][0] = currentHypothesisComponent;
+            result[i][1] = currentHypothesisState;
+        }
+        return result;
+    }
+
+    private int countHypothesis() throws JessException{
+        int result = 0;
+        jess.QueryResult hypothesis = generateHypothesis();
+        while(selectHypothesis(hypothesis)){
+            result++;
+        }
+        return result;
+    }
+
+    private jess.QueryResult generateHypothesis() throws JessException {
+        return jess.runQueryStar("search-hypothesis", new jess.ValueVector());
+    }
+
+    private boolean selectHypothesis(jess.QueryResult hypothesis) throws JessException {
         if (hypothesis.next()) {
             currentHypothesisComponent = hypothesis.getString("component");
             currentHypothesisState = hypothesis.getString("state");
@@ -34,58 +86,6 @@ public class CRA {
         } else { return false; }
     }
 
-    private void negotiateObservable() throws JessException {
-	WorkingMemoryMarker beforeHypothesis = jess.mark();
-        c.printf("Trying hypothesis\n");
-        jess.assertString(
-            "(hypothesis " +
-            currentHypothesisComponent + " " + 
-            currentHypothesisState + ")"
-        );
-        jess.run();
-        //printFacts();
-        c.printf("Querying Observables\n");
-	jess.QueryResult observables =
-	    jess.runQueryStar("search-observable", new jess.ValueVector());
-        String answer = "no";
-	while (observables.next() && answer.equals("no")) {
-            String observable = observables.getString("observable");
-            c.printf(
-                "You want to observe " +
-                observable + "? true/false/no\n"
-            );
-            boolean noAnswer = true;
-            while (noAnswer) {
-                answer = c.readLine();
-                if (answer.equals("true")) {
-                    jess.resetToMark(beforeHypothesis);
-                    jess.assertString(
-                        "(observed " +
-                        observable + 
-                        " TRUE)"
-                    );
-                    jess.run();
-                    return;
-                } else if (answer.equals("false")) {
-                    jess.resetToMark(beforeHypothesis);
-                    jess.assertString(
-                        "(observed " +
-                        observable + 
-                        " FALSE)"
-                    );
-                    jess.run();
-                    return;
-                } else if (answer.equals("no")) {
-                    //
-                } else {
-                    c.printf("try again: true/false/no\n");
-                    continue;
-                }
-                noAnswer = false;
-            }
-	}
-	c.printf("No observables for this hypothesis \n");
-    }
     private void printHypothesis() {
         c.printf(
             "The hypothesis is that " +
@@ -106,9 +106,80 @@ public class CRA {
         }
     }
 
-    public CRA() {
+    private void negotiateObservable() throws JessException {
+	WorkingMemoryMarker beforeHypothesis = jess.mark();
+        c.printf("Trying hypothesis\n");
+        jess.assertString(
+            "(hypothesis " +
+            currentHypothesisComponent + " " +
+            currentHypothesisState + ")"
+        );
+        jess.run();
+        //printFacts();
+        c.printf("Querying Observables\n");
+		jess.QueryResult observables =
+	    jess.runQueryStar("search-observable", new jess.ValueVector());
+        String answer = "no";
+		while (observables.next() && answer.equals("no")) {
+            String observable = observables.getString("observable");
+            c.printf(
+                "You want to observe " +
+                observable + "? true/false/no\n"
+            );
+            boolean noAnswer = true;
+            while (noAnswer) {
+                answer = c.readLine();
+                if (answer.equals("true")) {
+                    jess.resetToMark(beforeHypothesis);
+                    jess.assertString(
+                        "(observed " +
+                        observable +
+                        " TRUE)"
+                    );
+                    jess.run();
+                    return;
+                } else if (answer.equals("false")) {
+                    jess.resetToMark(beforeHypothesis);
+                    jess.assertString(
+                        "(observed " +
+                        observable +
+                        " FALSE)"
+                    );
+                    jess.run();
+                    return;
+                } else if (answer.equals("no")) {
+                    //
+                } else {
+                    c.printf("try again: true/false/no\n");
+                    continue;
+                }
+                noAnswer = false;
+            }
+	}
+	c.printf("No observables for this hypothesis \n");
+    }
+    private void printHypothesis() {
+         c.printf(
+             "The hypothesis is that " +
+             currentHypothesisComponent +
+             " is " + 
+             currentHypothesisState + "\n"
+         );
+     }
+ 
+     private void printFacts() {
+         try {
+             java.io.Writer co = new java.io.PrintWriter(System.out);
+             co.write("-----------------------------------\n");
+             jess.ppFacts(co);
+             co.flush();
+         } catch (java.io.IOException ex) {
+             System.err.println(ex);
+         }
+     }
+    public CRAjoost() {
         jess = new Rete();
-        c = System.console();
+        c = new ConsoleCheat();
         try {
             jess.batch("jess/test/negotiate-test.jess");
             jess.reset();
@@ -116,7 +187,9 @@ public class CRA {
             askComplaint();
             //printFacts();
             jess.run();
-            while(selectHypothesis()) {
+            jess.QueryResult hypothesis = generateHypothesis();
+            while(selectHypothesis(hypothesis)) {
+                askAvailableHypothesis();
                 printHypothesis();
                 negotiateObservable();
             }
@@ -124,8 +197,8 @@ public class CRA {
             System.err.println(ex);
         }
     }
-    
+
     public static void main(String[] arg) {
-        new CRA();
+        new CRAjoost();
     }
 }
